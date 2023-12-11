de85eb72770b6e550214e2623311d066492e533c
Paulo Barros <barrosp335@gmail.com>
2023-12-11 10:49:34 -0300

    meu  preimeiro commit

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -3,10 +3,10 @@ import styles from "./NewProject.module.css";
 
 function NewProject() {
   return (
-    <div className={styles.newproject_container}>
+    <div>
       <h1>Criar projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços </p>
-      <ProjectFrom />
+      <p>Fomulário</p>
     </div>
   );
 }
