fc7feae1fc7fb572e40d71b67210eb1fa55e5d91
Paulo Barros <barrosp335@gmail.com>
2023-12-11 12:20:52 -0300

    meu  segundo commit

--- src/components/Project/ProjectForm.js
+++ src/components/Project/ProjectForm.js
@@ -5,25 +5,43 @@ import styles from "./ProjectForm.module.css";
 
 function ProjectForm() {
   return (
-    <form className={styles.form}>
-      <Input
-        type="text"
-        text="Nome do projeto"
-        name="name"
-        placeholder="Insira o nome do projeto"
-      />
-      <Input
-        type="number"
-        text="Orçamento do projeto"
-        name="budget"
-        placeholder="Insira o orçanento total"
-      />
-      <Select name="category_id" text="Selecione a categoria" />
+    <form>
+      <div>
+        <input type="text" placeholder="Isira o no nome do projeto" />
+      </div>
+
+      <div>
+        <input type="number" placeholder="Insira o orçamento total" />
+      </div>
+
+      <div>
+        <select name="category_id">
+          <option disabled>Selecione a categoria</option>
+        </select>
+      </div>
       <div>
         <input type="submit" value="Criar projeto" />
       </div>
     </form>
   );
+  // <form className={styles.form}>
+  //   <Input
+  //     type="text"
+  //     text="Nome do projeto"
+  //     name="name"
+  //     placeholder="Insira o nome do projeto"
+  //   />
+  //   <Input
+  //     type="number"
+  //     text="Orçamento do projeto"
+  //     name="budget"
+  //     placeholder="Insira o orçanento total"
+  //   />
+  //   <Select name="category_id" text="Selecione a categoria" />
+  //   <div>
+  //     <input type="submit" value="Criar projeto" />
+  //   </div>
+  // </form>
 }
 
 export default ProjectForm;
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -3,10 +3,10 @@ import styles from "./NewProject.module.css";
 
 function NewProject() {
   return (
-    <div>
+    <div className={styles.newproject_container}>
       <h1>Criar projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços </p>
-      <p>Fomulário</p>
+      <ProjectFrom />
     </div>
   );
 }
