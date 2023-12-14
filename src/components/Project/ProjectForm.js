d4d1df84f049bfcda325cc99bc50c76a24028076
Paulo Barros <barrosp335@gmail.com>
2023-12-14 12:55:31 -0300

    Inserindo dados no banco via API

--- src/components/Project/ProjectForm.js
+++ src/components/Project/ProjectForm.js
@@ -6,8 +6,9 @@ import SubmitButton from "../form/SubmitButton";
 
 import styles from "./ProjectForm.module.css";
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
   const [categories, setCategories] = useState([]);
+  const [project, setProject] = useState(projectData || {});
 
   useEffect(() => {
     fetch("http://localhost:5000/categories", {
@@ -23,25 +24,51 @@ function ProjectForm({ btnText }) {
       .catch((err) => console.log(err));
   }, []);
 
+  const submit = (e) => {
+    e.preventDefault();
+    // console.log(project);
+    handleSubmit(project);
+  };
+
+  function handleChange(e) {
+    setProject({ ...project, [e.target.name]: e.target.value });
+  }
+
+  function handleCategory(e) {
+    setProject({
+      ...project,
+      category: {
+        id: e.target.value,
+        name: e.target.options[e.target.selectedIndex].text,
+      },
+    });
+  }
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nomedo projeto"
         name="name"
         placerholder="Insira o none do projeto"
+        handleOnchange={handleChange}
+        value={project.name ? project.name : ""}
       />
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placerholder="Insira o orçamento total"
+        handleOnchange={handleChange}
+        value={project.budget ? project.budget : ""}
       />
 
       <Select
         name="category_d"
         text="Selecione a categoria"
         options={categories}
+        handleOnchange={handleCategory}
+        value={project.category ? project.category.id : ""}
       />
       <SubmitButton text={btnText} />
     </form>
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,38 @@
+import { useNavigate } from "react-router-dom";
 import ProjectFrom from "../Project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 function NewProject() {
+  const navegar = useNavigate();
+
+  function createPost(project) {
+    // initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project, { message: "Projeto criado com sucesso!" }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+
+        navegar("/projects");
+      })
+      .catch((err) => console.log(err));
+
+    // redirect
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços </p>
-      <ProjectFrom btnText="Criar projeto" />
+      <ProjectFrom handleSubmit={createPost} btnText="Criar projeto" />
     </div>
   );
 }
